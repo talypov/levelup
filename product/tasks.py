93cb2a6998a11d9d7abb56b61c25c95cfb982112
ztalant <anton.talypov@sigma.software>
2023-03-13 22:13:35 +0200

    CR-3 Add model methods

--- product/tasks.py
+++ product/tasks.py
@@ -6,7 +6,6 @@ from celery import shared_task
 
 from levelup.celery import app
 from product.models import ProductListCSV, Order, Product
-from utils.parse_csv import is_new_product
 
 logger = logging.getLogger(__name__)
 
@@ -24,29 +23,19 @@ def parse_csv_task():
     else:
         with open(obj.file.path, 'r') as csv_file:
             data = list(csv.reader(csv_file, delimiter=";"))
-            rows = len(data[1:])
+            rows = len(data)
             one_row_per = 100 / rows
-            five = 5
             per_sum = one_row_per
 
-            for row in data[1:]:
+            for row in data:
                 csv_order = Order.objects.get_or_create(order_no=row[4])[0]
                 csv_order_products = list(csv_order.products.all().values_list('id', flat=True))
-                if len(csv_order_products) == 0 or is_new_product(row, csv_order_products):
-                    product = Product.objects.create(
-                        product_no=row[0],
-                        name=row[1],
-                        description=row[2],
-                        r_state=row[3],
-                        type=row[5],
-                        qty=row[6],
-                    )
+                if len(csv_order_products) == 0 or Product.is_new_product(row, csv_order_products):
+                    product = Product.from_csv(row)
                     csv_order.products.add(product)
 
-                if per_sum >= five:
-                    sys.stdout.write("[%-10s] %d%%" % ('=' * int(five / 5), per_sum))
-                    sleep(0.25)
-                    five += 5
+                sys.stdout.write("[%-10s] %d%%" % ('=' * int(per_sum/10), per_sum))
+                sleep(0.25)
                 per_sum += one_row_per
 
         obj.is_done = True
